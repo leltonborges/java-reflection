3285d1f05069559c71722a106b690b21e89af485
Lelton Borges <leltondev@gmail.com>
2021-12-30 14:23:24 -0300

    newInstance ProdutoController

--- alurator/src/br/com/alura/alurator/Alurator.java
+++ alurator/src/br/com/alura/alurator/Alurator.java
@@ -1,11 +1,32 @@
 package br.com.alura.alurator;
 
 public class Alurator {
-	
-	public Object executa(String url) {
-		// TODO - processa a requisicao executando o metodo
-		// da classe em questao
-		
-		return null;
-	}
+
+    private String pacoteBase;
+
+    public Alurator(String pacoteBase) {
+        this.pacoteBase = pacoteBase;
+
+    }
+
+    public Object executa(String url) {
+        // TODO - processa a requisicao executando o metodo
+
+        String[] parteUrl = url.replaceFirst("/", "")
+                .split("/");
+
+        String nomeController = Character.toUpperCase(parteUrl[0].charAt(0))
+                + parteUrl[0].substring(1)
+                + "Controller";
+
+        try {
+            Class<?> classeControle = Class.forName(pacoteBase + nomeController);
+            Object instanceController = classeControle.newInstance();
+
+            System.out.println(instanceController);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
